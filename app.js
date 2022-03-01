e6eaa0b647a2074ffe9cd5a7fa092eab5bc8696d
mohammad-mobarak-hossen <mdmobarakhossen112@gmail.com>
2022-03-02 01:39:01 +0600

    third

--- app.js
+++ app.js
@@ -6,21 +6,26 @@ document.getElementById('search-button').addEventListener('click', function name
 
     // Api start 
     let url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
-    console.log(url);
     fetch(url)
         .then(respons => respons.json())
         .then(data => displyPhone(data.data))
+
 });
 
 const displyPhone = (phones) => {
-    let divContainer = document.getElementById('phone-container')
-    divContainer.textContent = '';
-    for (let phone of phones.slice(0, 20)) {
-        // console.log(phone);
-        let div = document.createElement('div')
-        div.classList.add('col')
-        div.innerHTML = `<div class="card p-4">
-        <img  src="${phone.image}" class="card-img-top w-50 ms-5 bg-warning" alt="...">
+    if (phones == false) {
+        document.getElementById('error-case').style.display = 'block'
+    }
+    else {
+        document.getElementById('error-case').style.display = 'none'
+        let divContainer = document.getElementById('phone-container')
+        divContainer.textContent = '';
+        for (let phone of phones.slice(1, 20)) {
+            // console.log(phone);
+            let div = document.createElement('div')
+            div.classList.add('col')
+            div.innerHTML = `<div class="card p-4 rounded-3">
+        <img  src="${phone.image}" class="card-img-top w-50 mx-auto bg-warning" alt="...">
         <div class="card-body">
             <h4 class="card-title">${phone.brand}</h4>
             <h5>${phone.phone_name}</h5>
@@ -29,13 +34,13 @@ const displyPhone = (phones) => {
             <button onclick="showDetails('${phone.slug}')" class="btn btn-info">Show Details</button>
         </div>
     </div>`
-        divContainer.appendChild(div);
+            divContainer.appendChild(div);
+        }
     }
 };
 
 let showDetails = (phoneId) => {
     let url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
-    console.log(url);
     fetch(url)
         .then(respons => respons.json())
         .then(data => showDispleDetails(data.data))
@@ -55,6 +60,12 @@ let showDispleDetails = (details) => {
       <p class="card-title">Demory: ${details.mainFeatures.memory}</p>
       <p class="card-text">ReleaseDate: ${details.releaseDate ? details.releaseDate : 'ReleseDate Not Found'}</p>
       <p class="card-text">Sensors: ${details.mainFeatures.sensors}</p >
+      <p class="card-text">Bluetooth: ${details.others.Bluetooth}</p >
+      <p class="card-text">GPS: ${details.others.GPS}</p >
+      <p class="card-text">NFC: ${details.others.NFC}</p >
+      <p class="card-text">Radio: ${details.others.Radio}</p >
+      <p class="card-text">USB: ${details.others.USB}</p >
+      <p class="card-text">WLAN: ${details.others.WLAN}</p >
     </div >
   </div >
     `;
