8f2b6cbae8541bb4bcc42f5c17279aeaa0bd250b
mohammad-mobarak-hossen <mdmobarakhossen112@gmail.com>
2022-03-02 14:15:27 +0600

    last

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ document.getElementById('search-button').addEventListener('click', function name
     const searchInput = document.getElementById('search-input');
     const searchText = searchInput.value;
     searchInput.value = '';
-    // console.log(searchText);
 
     // Api start 
     let url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
@@ -13,6 +12,8 @@ document.getElementById('search-button').addEventListener('click', function name
 });
 
 const displyPhone = (phones) => {
+
+    // error case 
     if (phones == false) {
         document.getElementById('error-case').style.display = 'block'
     }
@@ -21,7 +22,6 @@ const displyPhone = (phones) => {
         let divContainer = document.getElementById('phone-container')
         divContainer.textContent = '';
         for (let phone of phones.slice(0, 20)) {
-            // console.log(phone);
             let div = document.createElement('div')
             div.classList.add('col')
             div.innerHTML = `<div class="card p-4 rounded-3">
@@ -46,27 +46,30 @@ let showDetails = (phoneId) => {
         .then(data => showDispleDetails(data.data))
 }
 
+//  showDetails disply 
 let showDispleDetails = (details) => {
     console.log(details);
 
     let showDisplyDiv = document.getElementById('show-details')
     showDisplyDiv.textContent = '';
     showDisplyDiv.innerHTML = `
-    <div class="card p-3" style="width: 18rem;">
-    <img src="${details.image}" class="card-img-top w-50 mx-auto" alt="...">
-    <div class="card-body">
-      <p class="card-title"><span class="fw-bold">ChipSet:</span> ${details.mainFeatures.chipSet}</hp>
-      <p class="card-title">${details.mainFeatures.displaySize}</p>
-      <p class="card-title">Demory:${details.mainFeatures.memory}</p>
-      <p class="card-text">ReleaseDate: ${details.releaseDate ? details.releaseDate : 'ReleseDate Not Found'}</p>
-      <p class="card-text">Sensors: ${details.mainFeatures.sensors}</p >
-      <p>Bluetooth: ${details.others.Bluetooth ? details.others.Bluetooth : 'Not Found'}</p >
-      <p>GPS: ${details.others.GPS ? details.others.GPS : 'Not Found'}</p >
-      <p>NFC: ${details.others.NFC ? details.others.NFC : 'Not Found'}</p >
-      <p>Radio: ${details.others.Radio ? details.others.Radio : 'Not Found'}</p >
-      <p>USB: ${details.others.USB ? details.others.USB : 'Not Found'}</p >
-      <p>WLAN: ${details.others.WLAN ? details.others.WLAN : 'Not Found'}</p >
-    </div >
-  </div >
-    `;
+        <div class="card p-3" style="width: 18rem;">
+        <img src="${details.image}" class="card-img-top w-50 mx-auto" alt="...">
+        <div class="card-body">
+          <p class="card-title"><span class="fw-bold">ChipSet:</span> ${details.mainFeatures.chipSet}</hp>
+          <p>${details.mainFeatures.displaySize}</p>
+          <p> <span class="fw-bold">Demory:</span>${details.mainFeatures.memory}</p>
+          <p> <span class="fw-bold"> ReleaseDate: </span> ${details.releaseDate ? details.releaseDate : 'ReleseDate Not Found'}</p>
+          <p><span class="fw-bold">Sensors:</span> ${details.mainFeatures.sensors}</p >
+          <p><span class="fw-bold">Bluetooth:</span> ${details.others?.Bluetooth ? details.others.Bluetooth : 'Not Found'}</p >
+          <p><span class="fw-bold">GPS:</span> ${details.others?.GPS ? details.others.GPS : 'Not Found'}</p >
+          <p><span class="fw-bold">NFC:</span> ${details.others?.NFC ? details.others.NFC : 'Not Found'}</p >
+          <p><span class="fw-bold">Radio:</span> ${details.others?.Radio ? details.others.Radio : 'Not Found'}</p >
+          <p><span class="fw-bold">USB:</span> ${details.others?.USB ? details.others.USB : 'Not Found'}</p >
+          <p><span class="fw-bold">WLAN:</span> ${details.others?.WLAN ? details.others.WLAN : 'Not Found'}</p >
+
+
+        </div >
+      </div >
+        `;
 }
